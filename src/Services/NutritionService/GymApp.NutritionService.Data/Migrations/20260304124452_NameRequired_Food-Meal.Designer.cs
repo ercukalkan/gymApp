de42f95dea8a19e58538e6eaa9e36c6f3c4021cf
under-construction <66276837+under-construction@users.noreply.github.com>
2026-03-04 15:46:45 +0300

    name required migration for food and meal

--- src/Services/NutritionService/GymApp.NutritionService.Data/Migrations/20260304124452_NameRequired_Food-Meal.Designer.cs
+++ src/Services/NutritionService/GymApp.NutritionService.Data/Migrations/20260304124452_NameRequired_Food-Meal.Designer.cs
@@ -4,6 +4,7 @@ using GymApp.NutritionService.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GymApp.NutritionService.Data.Migrations
 {
     [DbContext(typeof(NutritionContext))]
-    partial class NutritionContextModelSnapshot : ModelSnapshot
+    [Migration("20260304124452_NameRequired_Food-Meal")]
+    partial class NameRequired_FoodMeal
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Services/NutritionService/GymApp.NutritionService.Data/Migrations/20260304124452_NameRequired_Food-Meal.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GymApp.NutritionService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class NameRequired_FoodMeal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Meals",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Foods",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Meals",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Foods",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+        }
+    }
+}
